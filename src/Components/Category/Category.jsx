ff72e342e9b33b24070c2998334570c637f0ea28
Shawal <shahishawal@gmail.com>
2023-09-25 01:03:48 +0600

    Home props dril to category and showimh Card in ui

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,11 +1,33 @@
+import PropTypes from "prop-types";
 
-
-const Category = () => {
-    return (
-        <div>
-            
+const Category = ({ datas }) => {
+  const {
+    picture,
+    title,
+    category
+  } = datas;
+  return (
+    <div>
+      <div>
+        <div className="card card-compact  bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src={picture}
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{category}</h2>
+            <p>{title}</p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
+};
+
+Category.propTypes = {
+  datas: PropTypes.object.isRequired,
 };
 
 export default Category;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,11 +10,13 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <div>
+           <div className="mx-w-4xl mx-auto px-8">
+           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-12">
                 {
                     datas.map((datas)=> <Category key={datas.id} datas={datas}></Category>)
                 }
             </div>
+           </div>
         </div>
     );
 };
